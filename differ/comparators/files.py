027bd3c33a8058be9f86d815a26b1dff714eee88
Adam Meily <adam.meily@trailofbits.com>
2022-12-13 11:19:06 -0500

    add more checks (existance, file type, file mode) -- #32

--- differ/comparators/files.py
+++ differ/comparators/files.py
@@ -1,13 +1,33 @@
 import hashlib
+from enum import Enum
 from pathlib import Path
 from typing import Optional
 
 import ssdeep
 
-from differ.core import Comparator, ComparisonResult, CrashResult, Trace
+from differ.core import Comparator, ComparisonResult, CrashResult, Trace, VariableRef
 
 from . import register
 
+
+class PathType(Enum):
+    file = 'file'
+    directory = 'directory'
+
+
+class OctalRef(VariableRef):
+    """
+    An octal integer variable reference. This method always returns the octal representation of the
+    integer. For example, if a variable has a value of ``555`` (base 10), the value ``365`` is
+    returned (``int('555', 8) == 0o555 == 365``).
+    """
+
+    def get(self, values: dict) -> int:
+        value = values[self.variable]
+        return int(str(value), 8)
+
+
+MODE_NO_CHECK = -1
 
 @register('file')
 class FileComparator(Comparator):
@@ -31,17 +51,55 @@ class FileComparator(Comparator):
     def __init__(self, config: dict):
         super().__init__(config)
         self.filename: str = config['filename']
-        self.similarity: int = config.get('similarity', 100)
+        self.exists = config.get('exists', True)
+        self.path_type = PathType(config.get('type', 'file'))
+        mode = OctalRef.try_parse(config.get('mode'))
+
+        if isinstance(mode, int):
+            self.mode = int(str(mode), 8)
+        elif isinstance(mode, str) and mode:
+            self.mode = int(mode, 8)
+        elif isinstance(mode, OctalRef):
+            self.mode = mode
+        else:
+            self.mode = MODE_NO_CHECK
+
+        if not self.exists or self.path_type is PathType.directory:
+            self.similarity = 0
+        else:
+            self.similarity = config.get('similarity', 100)
 
     def verify_original(self, trace: Trace) -> Optional[CrashResult]:
         filename = trace.cwd / self.filename
-        if not filename.is_file():
-            return CrashResult(trace, f'file does not exist: {self.filename}', comparator=self)
+        if error := self.check_file_type(trace, filename):
+            return CrashResult(trace, error, comparator=self)
+
+        if not self.similarity:
+            return
 
         sha1, fuzzy = self.hash_file(filename)
 
         trace.cache[f'{self.filename}_sha1'] = sha1
         trace.cache[f'{self.filename}_ssdeep'] = fuzzy
+
+    def check_file_type(self, trace: Trace, filename: Path) -> Optional[str]:
+        if self.exists:
+            if not filename.exists():
+                return f'file does not exist: {self.filename}'
+
+            if self.path_type is PathType.file and not filename.is_file():
+                return f'path is not a normal file: {self.filename}'
+
+            if self.path_type is PathType.directory and not filename.is_dir():
+                return f'path is not a directory: {self.filename}'
+        elif filename.exists():
+            return f'file exists: {self.filename}'
+
+        expected_mode = OctalRef.deref(self.mode, trace.context.values)
+        if expected_mode != MODE_NO_CHECK and filename.exists():
+            mode = filename.stat().st_mode & 0o777
+            if expected_mode != mode:
+                return f'file mode does not match expected: mode={mode:o}, expected={expected_mode:o}'
 
     @classmethod
     def hash_file(cls, filename: Path) -> tuple[str, str]:
@@ -64,6 +122,12 @@ class FileComparator(Comparator):
 
     def compare(self, original: Trace, debloated: Trace) -> ComparisonResult:
         filename = debloated.cwd / self.filename
+        if error := self.check_file_type(debloated, filename):
+            return ComparisonResult.error(self, debloated, error)
+
+        if not self.similarity:
+            return ComparisonResult.success(self, debloated)
+
         sha1, fuzzy = self.hash_file(filename)
         if sha1 == original.cache[f'{self.filename}_sha1']:
             similarity = 100
