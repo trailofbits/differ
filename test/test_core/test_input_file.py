d328403eb227d255b5ae90a44a6b35b58854cfd1
Adam Meily <adam.meily@trailofbits.com>
2023-02-02 17:39:31 -0500

    lint

--- test/test_core/test_input_file.py
+++ test/test_core/test_input_file.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock, patch
 
 from differ import core
 
--- test/test_core/test_timeout_constraint.py
+++ test/test_core/test_timeout_constraint.py
@@ -3,10 +3,9 @@ from differ.core import TimeoutConstraint
 
 class TestTimeoutConstraint:
     def test_load_dict(self):
-        assert TimeoutConstraint.load_dict({
-            'seconds': 5,
-            'expected': True
-        }) == TimeoutConstraint(5, True)
+        assert TimeoutConstraint.load_dict({'seconds': 5, 'expected': True}) == TimeoutConstraint(
+            5, True
+        )
 
     def test_load_dict_int(self):
         assert TimeoutConstraint.load_dict(5) == TimeoutConstraint(5)
